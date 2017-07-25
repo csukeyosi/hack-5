1f018ba797f14b1ca344d8d46b91b6e88157929e
Carla Sukeyosi <carlasukeyosi@gmail.com>
2017-07-25 09:50:38 -0700

    centroid for 1 and 2 points

--- js/region.js
+++ js/region.js
@@ -22,6 +22,47 @@ Region.prototype.area = function () {
 };
 
 Region.prototype.centroid = function () {
+    if (this.length == 1) {
+        return this.points[0];
+    }
+
+    if (this.length == 2) {
+
+        //-- Define radius function
+        if (typeof (Number.prototype.toRad) === "undefined") {
+            Number.prototype.toRad = function () {
+                return this * Math.PI / 180;
+            }
+        }
+
+        //-- Define degrees function
+        if (typeof (Number.prototype.toDeg) === "undefined") {
+            Number.prototype.toDeg = function () {
+                return this * (180 / Math.PI);
+            }
+        }
+        var lat1 = this.points[0].x;
+        var lat2 = this.points[1].x;
+        var lng1 = this.points[0].y;
+        var lng2 = this.points[1].y;
+        //-- Longitude difference
+        var dLng = (lng2 - lng1).toRad();
+
+        //-- Convert to radians
+        lat1 = lat1.toRad();
+        lat2 = lat2.toRad();
+        lng1 = lng1.toRad();
+
+        var bX = Math.cos(lat2) * Math.cos(dLng);
+        var bY = Math.cos(lat2) * Math.sin(dLng);
+        var lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + bX) * (Math.cos(lat1) + bX) + bY * bY));
+        var lng3 = lng1 + Math.atan2(bY, Math.cos(lat1) + bX);
+
+        //-- Return result
+        return  new Point(lat3.toDeg(), lng3.toDeg());
+
+    }
+
     var x = 0,
         y = 0,
         i,
